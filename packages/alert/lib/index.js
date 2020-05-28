83fed0ec34d02287914436d9e0692b909e991929
wangjx2018 <wangjx2018@chianunicom.cn>
2020-05-28 16:53:51 +0800

    feat(alert 打包代码): alert 打包源码

---
+++ packages/alert/lib/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const prefixCls = 'happy-alert'
+
+const kinds = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502',
+};
+
+const Alert = ({ children, kind = 'info', ...rest }) => (
+  <div
+    className={prefixCls}
+    style={{
+      background: kinds[kind],
+    }}
+    {...rest}
+  >
+    {children}
+  </div>
+);
+
+export default Alert;
